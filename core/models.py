8089437e71bed14604618e1045db791512e83cee
leejeongmin <danmin20@khu.ac.kr>
2020-06-01 15:29:24 +0900

    room model

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class AbstractTimestamp(models.Model):
 
     """ Abstract Timestamp Model """
 
-    created = models.DateField()
-    updated = models.DateField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
 
     class Meta:
         abstract = True
